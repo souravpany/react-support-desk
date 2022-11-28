57651189d5683f1a3d39cfb76bc8100a9a2596ec
sourav.kumar.pany <sourav.kumar.pany@gds.ey.com>
2022-11-28 16:31:31 +0530

    1.Add connect to db details 2.Add routes and controller 3.Add error and exception handling 4.Add asyncHandler for asynchronous code execution

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,32 @@
-console.log('Server....');
+const express = require('express')
+require('dotenv').config()
+require('colors')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+//for error handling...
+const { errorHandler } = require('./middleware/errorMiddleware')
+
+// Connect to database
+connectDB()
+
+
+const app = express()
+
+// it allows us to pass json as parm..
+app.use(express.json())
+// it allows us to pass values as in url..
+app.use(express.urlencoded({ extended: false }))
+
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+
+
+app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+})
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
