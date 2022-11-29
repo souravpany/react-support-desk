b86ef05ca76557995e3a21968cc562d1616bf5bf
sourav.kumar.pany <sourav.kumar.pany@gds.ey.com>
2022-11-29 12:51:18 +0530

    Add Tickets and Notes functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
+import PrivateRoute from './components/PrivateRoute';
+
 import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import NewTicket from './pages/NewTicket';
+import Tickets from './pages/Tickets';
+import Ticket from './pages/Ticket'
 
 
 function App() {
@@ -18,6 +23,34 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+
+            <Route
+              path='/new-ticket'
+              element={
+                <PrivateRoute>
+                  <NewTicket />
+                </PrivateRoute>
+              }
+            />
+
+            <Route
+              path='/tickets'
+              element={
+                <PrivateRoute>
+                  <Tickets />
+                </PrivateRoute>
+              }
+            />
+
+            <Route
+              path='/ticket/:ticketId'
+              element={
+                <PrivateRoute>
+                  <Ticket />
+                </PrivateRoute>
+              }
+            />
+
           </Routes>
         </div>
       </Router>
